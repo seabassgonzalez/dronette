7514f99f899c94e2f14651072a56650e99e40c00
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-16 19:44:33 -0700

    Home and about routes instantiated, console logs to track rendering

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 5000;	
+
+const customMiddleware = (req, res, next) => {
+	console.log("Middleware executed");
+	next();
+};
+
+app.use(customMiddleware);
 
 app.get('/', (req, res) => {
+	console.log("home route executing");
 	res.send("hello world");
+});
+
+app.get('/about', (req, res) => {
+	console.log("about route executing");
+	res.send("about page");
 });
 
 app.listen(PORT, () => {
