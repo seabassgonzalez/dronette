c845c006a58d9956c5c0a1aba930919f93e77351
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-17 17:13:25 -0700

    Import mongoose and  MONGOURI, implement mongoose.connect calls including to handle error, implement mongoose.connection call to console log success or error of connection; remove unneeded <> from password in keys

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 5000;	
+const { MONGOURI } = require('./keys');
+
+mongoose.connect(MONGOURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+	console.log("Connected to mongo");
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log("Error connecting ", err);
+});
 
 const customMiddleware = (req, res, next) => {
 	console.log("Middleware executed");
--- keys.js
+++ keys.js
@@ -1,3 +1,3 @@
 module.exports={
-	MONGOURI: "mongodb+srv://seabassgonzalez:<12345>@cluster0.pytbu.mongodb.net/<dbname>?retryWrites=true&w=majority"
+	MONGOURI: "mongodb+srv://seabassgonzalez:12345@cluster0.pytbu.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
