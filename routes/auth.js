bb96561c16287fa8f2c5008e0c3868d5838c54c4
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-17 21:32:37 -0700

    Import bcrypt password hashing library; use bcrypt to hash user password

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
 	res.send("hello");
@@ -19,17 +20,20 @@ router.post('/signup', (req, res) => {
 		if(savedUser){
 			return res.status(422).json({error:"User already exists with that email"});
 		}
-		const user = new User({
-			email,
-			password,
-			name
-		});
-		user.save()
-		.then(user=>{
-			res.json({message:"Saved Successfully"});
-		})
-		.catch(err=>{
-			console.log(err);
+		bcrypt.hash(password, 12)
+		.then(hashedpassword=>{
+			const user = new User({
+				email,
+				password:hashedpassword,
+				name
+			});
+			user.save()
+			.then(user=>{
+				res.json({message:"Saved Successfully"});
+			})
+			.catch(err=>{
+				console.log(err);
+			});
 		});
 	})
 	.catch(err=>{
