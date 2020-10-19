111f54bc6576b3617a4fd2414e3d99d1db7db253
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-18 19:43:15 -0700

    Respond with token for sign in; install jsonwebtoken

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-	MONGOURI: "mongodb+srv://seabassgonzalez:12345@cluster0.pytbu.mongodb.net/<dbname>?retryWrites=true&w=majority"
+	MONGOURI: "mongodb+srv://seabassgonzalez:12345@cluster0.pytbu.mongodb.net/<dbname>?retryWrites=true&w=majority",
+	JWT_SECRET:"somesecretrandomstring"
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../keys');
 
 router.get('/', (req, res) => {
 	res.send("hello");
@@ -54,7 +56,8 @@ router.post('/signin', (req, res) => {
 		bcrypt.compare(password, savedUser.password)
 		.then(doMatch=>{
 			if(doMatch){
-				res.json({message:"Successfully signed in"});
+				const token = jwt.sign({_id:savedUser._id}, JWT_SECRET);
+				res.json({token});
 			} else {
 				return res.status(422).json({error:"Invalid email or password"});
 			}
