8d6cb907689534cad19a390c1ab6f7e61560ecfd
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-18 21:08:16 -0700

    Clear token if not authorized, verify token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,6 +5,7 @@ const User = mongoose.model("User");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {JWT_SECRET} = require('../keys');
+const requireLogin = require('../middleware/requireLogin');
 
 router.get('/', (req, res) => {
 	res.send("hello");
