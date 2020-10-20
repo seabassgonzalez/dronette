0d013cc2d7027b68196829acc875ed462c9edb41
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-19 19:33:42 -0700

    Refactor order of operations; initial post schema

--- server/app.js
+++ server/app.js
@@ -4,11 +4,6 @@ const mongoose = require('mongoose');
 const PORT = 5000;	
 const { MONGOURI } = require('./keys');
 
-require('./models/user');
-
-app.use(express.json());
-app.use(require('./routes/auth'));
-
 mongoose.connect(MONGOURI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -21,6 +16,11 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
 	console.log("Error connecting ", err);
 });
+
+require('./models/user');
+
+app.use(express.json());
+app.use(require('./routes/auth'));
 
 const customMiddleware = (req, res, next) => {
 	console.log("Middleware executed");
