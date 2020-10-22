229805b9aa38c715201b6d2711d4456fe0b4b63e
seabassgonzalez <seabassgonzalez@gmail.com>
2020-10-21 20:10:54 -0700

    Import post model, set password to undefined so that it's not saved in each post

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
-const Post =  mongoose.model("Post")
+const Post =  mongoose.model("Post");
 
 router.post('/createpost', requireLogin, (req, res) => {
 	const {title, body} = req.body;
 	if(!title || !body){
 		return res.status(422).json({error: "Please add all fields"});
 	}
-
+	req.user.password = undefined;
 	const post = new Post({
 		title,
 		body,
