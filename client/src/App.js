0f875f4ad2e1e60e9ef7cb73a2ec5ad77537d33f
seabassgonzalez <seabassgonzalez@gmail.com>
2020-11-03 16:00:17 -0800

    / route and /login route components to be rendered

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import NavBar from './components/Navbar';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/screens/Home';
+import Login from './components/screens/Login';
+import Profile from './components/screens/Profile';
+import Signup from './components/screens/Signup';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Route path="/">
-
+        <Home />
       </Route>
+      <Route path="/login">
+        <Login />
     </BrowserRouter>
   );
 }
