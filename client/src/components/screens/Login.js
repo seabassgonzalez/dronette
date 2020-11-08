f44f3edf1748998c12884c499a6a4ebf055e9cdb
seabassgonzalez <seabassgonzalez@gmail.com>
2020-11-07 21:24:50 -0800

    Update css; implement login screen; implement signup screen

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 	return (
 		<div className="mycard">
-			<div className="card auth-card">
+			<div className="card auth-card input-field">
 				<h2>Dronette</h2>
 				<input 
 					type="text"
@@ -13,9 +14,12 @@ const Login = () => {
 					type="text"
 					placeholder="password"
 				/>
-				<button className="btn waves-effect waves-light">
+				<button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
 					Login
 				</button>
+				<h6>
+					<Link to="/signup">Don't already have an account?</Link>
+				</h6>
 			</div>
 		</div>
 	);
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Signup = () => {
+const Login = () => {
 	return (
-		<h1>Signup</h1>
+		<div className="mycard">
+			<div className="card auth-card input-field">
+				<h2>Dronette</h2>
+				<input 
+					type="text"
+					placeholder="name"
+				/>
+				<input 
+					type="text"
+					placeholder="email"
+				/>
+				<input 
+					type="text"
+					placeholder="password"
+				/>
+				<button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+					Sign Up
+				</button>
+				<h6>
+					<Link to="/login">Already have an account?</Link>
+				</h6>
+			</div>
+		</div>
 	);
 };
 
-export default Signup;
+export default Login;
