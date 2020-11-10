421a56e83cff2300eadbb8950ef340a044183fc9
seabassgonzalez <seabassgonzalez@gmail.com>
2020-11-09 16:44:06 -0800

    Import stylesheet for icons; implement css for home-card; implement static home screen example

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -9,6 +9,43 @@ const Home = () => {
 					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
 				</div>
 				<div className="card-content">
+					<i class="material-icons" style={{color:"red"}}>favorite</i>
+					<h6>Title</h6>
+					<p>This amazing post</p>
+					<input type="text" placeholder="add a comment" />
+				</div>
+			</div>
+			<div className="card home-card">
+				<h5>Sebastian</h5>
+				<div className="card-image">
+					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
+				</div>
+				<div className="card-content">
+					<i class="material-icons" style={{color:"red"}}>favorite</i>
+					<h6>Title</h6>
+					<p>This amazing post</p>
+					<input type="text" placeholder="add a comment" />
+				</div>
+			</div>
+			<div className="card home-card">
+				<h5>Sebastian</h5>
+				<div className="card-image">
+					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
+				</div>
+				<div className="card-content">
+					<i class="material-icons" style={{color:"red"}}>favorite</i>
+					<h6>Title</h6>
+					<p>This amazing post</p>
+					<input type="text" placeholder="add a comment" />
+				</div>
+			</div>
+			<div className="card home-card">
+				<h5>Sebastian</h5>
+				<div className="card-image">
+					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
+				</div>
+				<div className="card-content">
+					<i class="material-icons" style={{color:"red"}}>favorite</i>
 					<h6>Title</h6>
 					<p>This amazing post</p>
 					<input type="text" placeholder="add a comment" />
