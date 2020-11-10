88fb04061dd5896232e4fac1d331b9233c45b9a3
seabassgonzalez <seabassgonzalez@gmail.com>
2020-11-09 19:34:01 -0800

    Implement createPost fields

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const createPost = () => {
 	return (
-
+		<div className="card input-field">
+			<input type="text" placeholder="title" />
+			<input type="text" placeholder="body" />
+			<div className="file-field input-field">
+				<div className="btn">
+					<span>File</span>
+					<input type="file" />
+				</div>
+				<div className="file-path-wrapper">
+					<input className="file-path validate" type="text" />
+				</div>
+			</div>
+		</div>
 	);
 };
 
