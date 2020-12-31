3f851fdc271a21c4918f794317ca48ee94afaa3b
seabassgonzalez <seabassgonzalez@gmail.com>
2020-12-30 18:36:45 -0800

    Implement Login logic

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,7 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import M from 'materialize-css';
 
 const Login = () => {
+	const history = useHistory();
+	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const PostData = () => {
+		if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+			M.toast({html: "invalid email", classes: "#c62828 red darken-3"});
+			return;
+		}
+		fetch("/login", {
+			method:"post",
+			headers:{
+				"Content-Type":"application/json"
+			},
+			body:JSON.stringify({
+				password,
+				email
+			})
+		}).then(res=>res.json())
+			.then(data=>{
+				console.log(data);
+				if(data.error){
+					M.toast({html: data.error, classes: "#c62828 red darken-1"});
+				} else {
+					M.toast({html: "signed in successfully", classes: "#43a047 green darken-1"});
+					history.push('/');
+				}
+			}).catch(err=>{
+				console.log(err);
+			});
+	}
+
 	return (
 		<div className="mycard">
 			<div className="card auth-card input-field">
@@ -9,12 +41,18 @@ const Login = () => {
 				<input 
 					type="text"
 					placeholder="email"
+					value={email}
+					onChange={(e)=>setEmail(e.target.value)}
 				/>
 				<input 
 					type="text"
 					placeholder="password"
+					value={password}
+					onChange={(e)=>setPassword(e.target.value)}
 				/>
-				<button className="btn waves-effect waves-light #9b59b6 purple lighten-2">
+				<button className="btn waves-effect waves-light #9b59b6 purple lighten-2"
+				onClick={()=>PostData()}
+				>
 					Login
 				</button>
 				<h6>
