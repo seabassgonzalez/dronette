fe8e2490b5d9b7849a673f45d9bc2b4d11e2a5af
seabassgonzalez <seabassgonzalez@gmail.com>
2021-01-01 20:56:43 -0800

    Instantiate image and url pieces of state, create postDetails to handle file submission

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -3,7 +3,27 @@ import React, {useState} from 'react';
 const CreatePost = () => {
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
-	const [image, setImage] = useState(""); 
+	const [image, setImage] = useState("");
+	const [url, setUrl] = useState(""); 
+
+	const postDetails = () => {
+		const data = new FormData();
+		data.append("file", image);
+		data.append("upload_preset", "dronette");
+		data.append("cloud_name", "dronette");
+		fetch("https://api.cloudinary.com/v1_1/dronette/image/upload", {
+			method: "post",
+			body:data
+		})
+		.then(res=>res.json())
+		.then(data=>{
+			console.log(data);
+		})
+		.catch(err=>{
+			console.log(err);
+		});
+	};
+
    	return (
 		<div className="card input-field"
 			style={{
@@ -28,13 +48,15 @@ const CreatePost = () => {
 			<div className="file-field input-field">
 				<div className="btn purple lighten-2">
 					<span>Upload Image</span>
-					<input type="file" onChange={(e)=>console.log(e.target.files)} />
+					<input type="file" onChange={(e)=>setImage(e.target.files[0])} />
 				</div>
 				<div className="file-path-wrapper">
 					<input className="file-path validate" type="text" />
 				</div>
 			</div>
-			<button className="btn waves-effect waves-light #9b59b6 purple lighten-2">
+			<button className="btn waves-effect waves-light #9b59b6 purple lighten-2"
+				onClick={()=>postDetails()}
+			>
 				Submit Post
 			</button>
 		</div>
