3536ab5401da746e48175f7ec66723f8e7dd5711
seabassgonzalez <seabassgonzalez@gmail.com>
2021-01-27 23:53:44 -0800

    Move routes out of BrowserRouter to wrap in Switch and be able to access history

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, createContext} from 'react';
 import NavBar from './components/Navbar';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/screens/Home';
 import Login from './components/screens/Login';
 import Profile from './components/screens/Profile';
@@ -10,10 +10,9 @@ import CreatePost from './components/screens/CreatePost';
 
 const userContext = createContext();
 
-function App() {
+const Routing = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
+    <Switch>
       <Route exact   path="/">
         <Home />
       </Route>
@@ -29,6 +28,15 @@ function App() {
       <Route path="/create">
         <CreatePost />
       </Route>
+    </Switch>
+  );
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
     </BrowserRouter>
   );
 }
