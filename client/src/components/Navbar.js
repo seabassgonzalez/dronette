02debb3b540c1390b60b5d0149762f4ef509247c
seabassgonzalez <seabassgonzalez@gmail.com>
2021-02-14 12:49:21 -0800

    Check if signed in to render one set of navbar links or another

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import {UserContext} from '../App';
 
 const NavBar = () => {
+  const {state, dispatch} = useContext(UserContext);
+  const renderList = () => {
+    if(state){
+      return [
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/create">Create Post</Link></li>
+      ]
+    }else{
+      return [
+        <li><Link to="/login">Login</Link></li>,
+        <li><Link to="/signup">Signup</Link></li>
+      ]
+    }
+  }
 	return(
 		<nav>
     		<div className="nav-wrapper white">
       			<Link to="/" className="brand-logo left">Dronette</Link>
       			<ul id="nav-mobile" className="right hide-on-med-and-down">
-        			<li><Link to="/login">Login</Link></li>
-        			<li><Link to="/signup">Signup</Link></li>
-        			<li><Link to="/profile">Profile</Link></li>
-              <li><Link to="/create">Create Post</Link></li>
+        		{renderList()}
       			</ul>
     		</div>
   		</nav>
