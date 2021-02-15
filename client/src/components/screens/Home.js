0bf7f0568ad764b60e75b1725a835633fc2013ef
seabassgonzalez <seabassgonzalez@gmail.com>
2021-02-14 16:15:06 -0800

    Place home-card structure within a map iterating over each item, reference data from each item in populating card

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -8,58 +8,33 @@ const Home = () => {
 				"Authorization":"Bearer " + localStorage.getItem("jwt")
 			}
 		}).then(res=>res.json())
-		.then
+		.then(result=>{
+			console.log(result);
+			setData(result.posts);
+		});		
 	}, []);
 	return (
 		<div className="home">
-			<div className="card home-card">
-				<h5>Sebastian</h5>
-				<div className="card-image">
-					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
-				</div>
-				<div className="card-content">
-					<i className="material-icons" style={{color:"red"}}>favorite</i>
-					<h6>Title</h6>
-					<p>This amazing post</p>
-					<input type="text" placeholder="add a comment" />
-				</div>
-			</div>
-			<div className="card home-card">
-				<h5>Sebastian</h5>
-				<div className="card-image">
-					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
-				</div>
-				<div className="card-content">
-					<i className="material-icons" style={{color:"red"}}>favorite</i>
-					<h6>Title</h6>
-					<p>This amazing post</p>
-					<input type="text" placeholder="add a comment" />
-				</div>
-			</div>
-			<div className="card home-card">
-				<h5>Sebastian</h5>
-				<div className="card-image">
-					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
-				</div>
-				<div className="card-content">
-					<i className="material-icons" style={{color:"red"}}>favorite</i>
-					<h6>Title</h6>
-					<p>This amazing post</p>
-					<input type="text" placeholder="add a comment" />
-				</div>
-			</div>
-			<div className="card home-card">
-				<h5>Sebastian</h5>
-				<div className="card-image">
-					<img src="https://images.unsplash.com/photo-1602548568341-13511c551dcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
-				</div>
-				<div className="card-content">
-					<i className="material-icons" style={{color:"red"}}>favorite</i>
-					<h6>Title</h6>
-					<p>This amazing post</p>
-					<input type="text" placeholder="add a comment" />
-				</div>
-			</div>
+			{
+				data.map(item=>{	
+					return(
+						<div className="card home-card" key={item._id}>
+							<h5>{item.postedBy.name}</h5>
+							<div className="card-image">
+								<img src={item.photo} />
+							</div>
+							<div className="card-content">
+								<i className="material-icons" style={{color:"red"}}>favorite</i>
+								<h6>{item.title}</h6>
+								<p>{item.body}</p>
+								<input type="text" placeholder="add a comment" />
+							</div>
+						</div>
+					);
+
+				})
+			}
+			
 		</div>
 	);
 };
