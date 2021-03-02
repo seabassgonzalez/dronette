0c1e7b1e2f91a50278c8416dd2699e50c6486db9
seabassgonzalez <seabassgonzalez@gmail.com>
2021-03-02 14:37:20 -0800

    Implement handling result of fetch to like route, using setData from useState

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -26,7 +26,15 @@ const Home = () => {
 			})
 		}).then(res=>res.json())
 		.then(result=>{
-			console.log(result)
+			console.log(result);
+			const newData = data.map(item=>{
+				if(item._id == result._id){
+					return result;
+				}else{
+					return item;
+				}
+			})
+			setData(newData);
 		})
 	};
 
