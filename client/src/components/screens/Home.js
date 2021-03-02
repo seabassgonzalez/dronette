709b1401e10ca131dbc1736f7cdb8b83927b6c2e
seabassgonzalez <seabassgonzalez@gmail.com>
2021-03-01 22:09:18 -0800

    Implement unlikePost and onClick for thumbs up and down

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -30,6 +30,22 @@ const Home = () => {
 		})
 	};
 
+	const unlikePost = (id) => {
+		fetch('/like', {
+			method: "put", 
+			headers: {
+				"Content-Type":"application/json",
+				"Authorization":"Bearer " + localStorage.getItem("jwt")
+			},
+			body: JSON.stringify({
+				postId:id
+			})
+		}).then(res=>res.json())
+		.then(result=>{
+			console.log(result)
+		})
+	};
+
 	return (
 		<div className="home">
 			{
@@ -42,8 +58,12 @@ const Home = () => {
 							</div>
 							<div className="card-content">
 								<i className="material-icons" style={{color:"red"}}>favorite</i>
-								<i className="material-icons">thumb_up</i>
-								<i className="material-icons">thumb_down</i>
+								<i className="material-icons"
+									onClick={()=>{likePost(item._id)}}
+								>thumb_up</i>
+								<i className="material-icons"
+									onClick={()=>{unlikePost(item._id)}}
+								>thumb_down</i>
 								<h6>{item.likes.length} likes</h6>
 								<h6>{item.title}</h6>
 								<p>{item.body}</p>
