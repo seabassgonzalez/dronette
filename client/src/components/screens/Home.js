e31b48453e17e1207fefb0989a2282def453a4d7
seabassgonzalez <seabassgonzalez@gmail.com>
2021-03-04 21:38:44 -0800

    Implement makeComment, render comments

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -81,6 +81,33 @@ const Home = () => {
 		})
 	};
 
+	const makeComment = (text, postId) => {
+		fetch('/comment', {
+			method: "put",
+			headers: {
+				"Content-Type":"application/json",
+				"Authorization":"Bearer " + localStorage.getItem("jwt")
+			},
+			body: JSON.stringify({
+				postId,
+				text
+			})
+		}).then(res=>res.json())
+		.then(result => {
+			console.log(result);
+			const newData = data.map(item=>{
+				if(item._id == result._id){
+					return result;
+				}else{
+					return item;
+				}
+			})
+			setData(newData);
+		}).catch(err=>{
+			console.log(err);
+		})
+	};
+
 	return (
 		<div className="home">
 			{
@@ -106,7 +133,20 @@ const Home = () => {
 								<h6>{item.likes.length} likes</h6>
 								<h6>{item.title}</h6>
 								<p>{item.body}</p>
-								<input type="text" placeholder="add a comment" />
+								{
+									item.comments.map(record=>{
+										return(
+											<h6><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+										);
+									})
+								}
+								<form onSubmit={(e)=>{
+									e.preventDefault()
+									console.log(e.target[0].value)
+									makeComment(e.target[0].value, item._id)
+								}}>
+									<input type="text" placeholder="add a comment" />
+								</form>
 							</div>
 						</div>
 					);
