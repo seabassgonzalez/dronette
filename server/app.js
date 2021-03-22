dc75485bb644ad88e48d052bfe5360af6e4ee6d9
seabassgonzalez <seabassgonzalez@gmail.com>
2021-03-21 21:03:07 -0700

    Implement user route to get user by id endpoint

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ require('./models/post');
 app.use(express.json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
+app.use(require('./routes/user'));
 
 const customMiddleware = (req, res, next) => {
 	console.log("Middleware executed");
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,26 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const Post =  mongoose.model("Post");
+const User = mongoose.model("User");
 
+// find user
+	// find posts created by user
 
+reouter.get('/user/:id', (req, res) => {
+	User.findOne({_id:req.param.id})
+	.select("-password")
+	.then(user=>{
+		Post.find({postedBy:req.param.id})
+		.populate("postedBy","_id name")
+		.exec((err, posts)=>{
+			if(err){
+				return res.status(422).json({error:err});
+			}
+			res.json({user, posts});
+		})
+	}).catch(err=>{
+		return res.status(404).json({error: "User not found"});
+	})
+});
 
 module.exports = router;
