05575406d70bae4ad324a48e83f99f0512595e52
seabassgonzalez <seabassgonzalez@gmail.com>
2021-04-25 21:46:34 -0700

    debugging heroku error

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@
 // else
 	// export dev
 if(process.env.NODE_ENV==='production'){
-	module.exports = require('./prod');
+    module.exports = require('./prod')
 }else{
-	module.exports = require('./dev');
+    module.exports = require('./dev')
 }
