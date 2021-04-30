204def13e86148964f85aa4ac1992785b4a18796
seabassgonzalez <seabassgonzalez@gmail.com>
2021-04-29 20:11:29 -0700

    initial test boilerplate and install chai and chai-http

---
+++ test/auth.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const auth = require('../routes/auth');
+
+describe('auth routes', function(){
+	it('should return message saved succesfully', function(){
+		// call function
+
+		// make assertion
+	})
+});
